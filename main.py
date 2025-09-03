feb40cd6c1e21fd69253653a9bb617b0b41a9603
Aneesha <aneeshadsouza2002@gmail.com>
2025-09-03 11:50:31 +0530

    Added Expense model and User-Expense relationship

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from database import Base, engine
 
 app = FastAPI()
+
+
+# create tables
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
---
+++ models.py
@@ -0,0 +1,24 @@
+# models.py
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+
+    expenses = relationship("Expense", back_populates="owner")
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float)
+    description = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="expenses")
